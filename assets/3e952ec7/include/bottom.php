be94277efb74193594fec94d164e216f738881b5
BRIGHTPC\Bright <qupelor21@gmail.com>
2017-09-25 16:33:29 +0700

    Commit

---
+++ assets/3e952ec7/include/bottom.php
@@ -0,0 +1,11 @@
+<!-- Go To Top Link -->
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <div id="loader">
+    <div class="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+  </div>
+
+  <script type="text/javascript" src="js/script.js"></script>
---
+++ assets/cd28e8d1/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row footer-widgets">
+          <div class="col-md-12">
+            <p class="text-center">กระทรวงยุติธรรม อาคารศูนย์ราชการเฉลิมพระเกียรติ ๘๐ พรรษา ๕ ธันวาคม ๒๕๕๐ เลขที่๑๒๐ หมู่๓ อาคาร A ถนนแจ้งวัฒนะ แขวงทุ่งสองห้อง เขตหลักสี่ กรุงเทพมหานคร ๑๐๒๑๐</p>
+          </div>
+        </div>
+        <!-- .row -->
+    </div>
+</footer>
