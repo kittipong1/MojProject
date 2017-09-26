e3be246a420666306c9f70e6f4a80afdd4b330d6
TOPTPS\TJWin10x64 22/9/2560 <toptps@hotmail.co.th>
2017-09-26 13:53:44 +0700

    ทดสอบ

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -14,12 +14,27 @@ class AppAsset extends AssetBundle
     public $css = [
         '../backend/web/css/site.css',
         '../backend/web/css/datepicker.css',
-        '../backend/web/css/editor.css',
+        '../backend/web/js/skins/moono-lisa/editor.css',
+        '../backend/web/js/plugins/scayt/skins/moono-lisa/scayt.css',
+        '../backend/web/js/plugins/tableselection/styles/tableselection.css',
+        '../backend/web/js/plugins/wsc/skins/moono-lisa/wsc.css',
+        '../backend/web/js/plugins/scayt/dialogs/dialog.css',
+        '../backend/web/js/skins/moono-lisa/dialog.css',
     ];
     public $js = [
         '../backend/web/js/bootstrap-datepicker.js',
         '../backend/web/js/ckeditor.js',
         '../backend/web/js/config.js',
+        '../backend/web/js/th.js',
+        '../backend/web/js/styles.js',
+        '../backend/web/js/plugins/image/dialogs/image.js',
+        '../backend/web/js/plugins/table/dialogs/table.js',
+        '../backend/web/js/plugins/specialchar/dialogs/lang/th.js',
+        '../backend/web/js/plugins/specialchar/dialogs/specialchar.js',
+        '../backend/web/js/plugins/link/dialogs/link.js',
+        '../backend/web/js/plugins/link/dialogs/anchor.js',
+        '../backend/web/js/plugins/about/dialogs/about.js',
+        '../backend/web/js/plugins/wsc/dialogs/wsc.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
