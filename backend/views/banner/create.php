b6aabbf8a70ba2a91442e9c569eee3f1089ead9f
TOPTPS\TJWin10x64 22/9/2560 <toptps@hotmail.co.th>
2017-09-27 14:59:35 +0700

    zzzz

--- backend/views/banner/create.php
+++ backend/views/banner/create.php
@@ -11,9 +11,9 @@ $this->params['breadcrumbs'][] = ['label' => 'Banners', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="box box-primary box-solid">
-	<div class="box-header">
+
     	<h1 class="box-title"><?= Html::encode($this->title) ?></h1>
-	</div>
+    	
 	<div class="box-body">
     	<?= $this->render('_form', [
         'model' => $model,
